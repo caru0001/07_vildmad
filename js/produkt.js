cd82325545086dfab74347807a715e94ab77f88a
SiljeSeneberg <silj3299@tud.kea.dk>
2024-02-20 15:23:13 +0100

    urlparams

--- js/produkt.js
+++ js/produkt.js
@@ -1,13 +1,21 @@
 // https://pliavlkcgnwhouapeprm.supabase.co
 // eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBsaWF2bGtjZ253aG91YXBlcHJtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDc0ODM2OTksImV4cCI6MjAyMzA1OTY5OX0.nkykaEzPhV7L-q--3enzm5iphogeP_xz2_IcvgkKhuM
 
-fetch("https://pliavlkcgnwhouapeprm.supabase.co/rest/v1/vildmad_data", {
-  method: "GET",
-  headers: {
-    apikey:
-      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBsaWF2bGtjZ253aG91YXBlcHJtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDc0ODM2OTksImV4cCI6MjAyMzA1OTY5OX0.nkykaEzPhV7L-q--3enzm5iphogeP_xz2_IcvgkKhuM",
-  },
-})
+/* her henter én eller flere specifikke data */
+const urlParms = new URLSearchParams(window.location.search);
+const id = urlParms.get("id");
+
+/* i fetch url definerer vi i dette tilfælde at det er "id" der skal hentes */
+fetch(
+  `https://pliavlkcgnwhouapeprm.supabase.co/rest/v1/vildmad_data?id=eq.${id}`,
+  {
+    method: "GET",
+    headers: {
+      apikey:
+        "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBsaWF2bGtjZ253aG91YXBlcHJtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDc0ODM2OTksImV4cCI6MjAyMzA1OTY5OX0.nkykaEzPhV7L-q--3enzm5iphogeP_xz2_IcvgkKhuM",
+    },
+  }
+)
   .then((res) => res.json())
   .then(showData);
 
@@ -20,18 +28,18 @@ function showData(item) {
   /* lav en kopi */
   const copy = template.cloneNode(true);
 
-  /* ændre indhold i template */
-  copy.querySelector("#urtPåCirkel").src = `img/herbs/${item.urtImg}`;
+  /* ændre indhold i template */ /* [0] fortæller den kun har en plads */
+  copy.querySelector("#urtPåCirkel").src = `img/herbs/${item[0].urtImg}`;
 
-  copy.querySelector("h1").textContent = item.name;
+  copy.querySelector("h1").textContent = item[0].name;
 
-  copy.querySelector(".introTekst").textContent = item.introduction;
+  copy.querySelector(".introTekst").textContent = item[0].introduction;
 
-  copy.querySelector(".rødTekst").textContent = item.look_description;
+  copy.querySelector(".rødTekst").textContent = item[0].look_description;
 
-  copy.querySelector(".grønTekst").textContent = item.area_description;
+  copy.querySelector(".grønTekst").textContent = item[0].area_description;
 
-  copy.querySelector("#area").textContent = item.areas;
+  copy.querySelector("#area").textContent = item[0].areas;
 
   /* append */
   document.querySelector("main").appendChild(copy);
--- js/produktliste.js
+++ js/produktliste.js
@@ -1,7 +1,11 @@
-fetch("https://pliavlkcgnwhouapeprm.supabase.co/rest/v1/vildmad_data", {
+const urlParms = new URLSearchParams(window.location.search);
+const season = urlParms.get("season");
+
+fetch(`https://pliavlkcgnwhouapeprm.supabase.co/rest/v1/vildmad_data?season=eq.${season}`, {
   method: "GET",
   headers: {
-    apikey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBsaWF2bGtjZ253aG91YXBlcHJtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDc0ODM2OTksImV4cCI6MjAyMzA1OTY5OX0.nkykaEzPhV7L-q--3enzm5iphogeP_xz2_IcvgkKhuM",
+    apikey:
+      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBsaWF2bGtjZ253aG91YXBlcHJtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDc0ODM2OTksImV4cCI6MjAyMzA1OTY5OX0.nkykaEzPhV7L-q--3enzm5iphogeP_xz2_IcvgkKhuM",
   },
 })
   .then((res) => res.json())
@@ -21,8 +25,9 @@ function showHerb(item) {
   const copy = template.cloneNode(true);
 
   /* ændre indhold i template */
-  copy.querySelector("img").src = `img/herbs/${item.urtImg}`;
-  copy.querySelector("h3").textContent = item.name;
+  copy.querySelector("img").src = `img/herbs/${item[0].urtImg}`;
+  copy.querySelector("h3").textContent = item[0].name;
+  copy.querySelector("a").href = `produkt.html?id=${item[0].id}`;
 
   /* append */
   document.querySelector(".herbs_grid").appendChild(copy);
