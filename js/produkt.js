347e77169e9c6817f691dff2e966e3bf10ac4a4e
Camilla <caru0001@stud.kea.dk>
2024-02-21 22:16:05 +0100

    styling

--- js/produkt.js
+++ js/produkt.js
@@ -6,16 +6,12 @@ const urlParms = new URLSearchParams(window.location.search);
 const id = urlParms.get("id");
 
 /* i fetch url definerer vi i dette tilfælde at det er "id" der skal hentes */
-fetch(
-  `https://pliavlkcgnwhouapeprm.supabase.co/rest/v1/vildmad_data?id=eq.${id}`,
-  {
-    method: "GET",
-    headers: {
-      apikey:
-        "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBsaWF2bGtjZ253aG91YXBlcHJtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDc0ODM2OTksImV4cCI6MjAyMzA1OTY5OX0.nkykaEzPhV7L-q--3enzm5iphogeP_xz2_IcvgkKhuM",
-    },
-  }
-)
+fetch(`https://pliavlkcgnwhouapeprm.supabase.co/rest/v1/vildmad_data?id=eq.${id}`, {
+  method: "GET",
+  headers: {
+    apikey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBsaWF2bGtjZ253aG91YXBlcHJtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDc0ODM2OTksImV4cCI6MjAyMzA1OTY5OX0.nkykaEzPhV7L-q--3enzm5iphogeP_xz2_IcvgkKhuM",
+  },
+})
   .then((res) => res.json())
   .then(showData);
 
@@ -39,7 +35,7 @@ function showData(item) {
 
   copy.querySelector(".grønTekst").textContent = item[0].area_description;
 
-  copy.querySelector("#area").textContent = item[0].areas;
+  //copy.querySelector("#area").textContent = item[0].areas;
 
   /* append */
   document.querySelector("main").appendChild(copy);
--- js/produktliste.js
+++ js/produktliste.js
@@ -1,18 +1,14 @@
 const urlParms = new URLSearchParams(window.location.search);
 const season = urlParms.get("season");
 
-/* `https://pliavlkcgnwhouapeprm.supabase.co/rest/v1/vildmad_data?season=eq.${season}` */
+/* Her siger vi hvis season er true, så fetch kun specifik sæson (som kommer fra url på index-siden) */
 if (season) {
-  fetch(
-    `https://pliavlkcgnwhouapeprm.supabase.co/rest/v1/vildmad_data?season=eq.${season}`,
-    {
-      method: "GET",
-      headers: {
-        apikey:
-          "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBsaWF2bGtjZ253aG91YXBlcHJtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDc0ODM2OTksImV4cCI6MjAyMzA1OTY5OX0.nkykaEzPhV7L-q--3enzm5iphogeP_xz2_IcvgkKhuM",
-      },
-    }
-  )
+  fetch(`https://pliavlkcgnwhouapeprm.supabase.co/rest/v1/vildmad_data?season=eq.${season}`, {
+    method: "GET",
+    headers: {
+      apikey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBsaWF2bGtjZ253aG91YXBlcHJtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDc0ODM2OTksImV4cCI6MjAyMzA1OTY5OX0.nkykaEzPhV7L-q--3enzm5iphogeP_xz2_IcvgkKhuM",
+    },
+  })
     .then((res) => res.json())
     .then(showData);
 
@@ -37,17 +33,19 @@ if (season) {
     /* append */
     document.querySelector(".herbs_grid").appendChild(copy);
   }
+
+  /* Her siger vi, hvis season er false/ikke bliver valgt, så vises al data (urter) */
 } else {
   fetch("https://pliavlkcgnwhouapeprm.supabase.co/rest/v1/vildmad_data", {
     method: "GET",
     headers: {
-      apikey:
-        "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBsaWF2bGtjZ253aG91YXBlcHJtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDc0ODM2OTksImV4cCI6MjAyMzA1OTY5OX0.nkykaEzPhV7L-q--3enzm5iphogeP_xz2_IcvgkKhuM",
+      apikey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBsaWF2bGtjZ253aG91YXBlcHJtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDc0ODM2OTksImV4cCI6MjAyMzA1OTY5OX0.nkykaEzPhV7L-q--3enzm5iphogeP_xz2_IcvgkKhuM",
     },
   })
     .then((res) => res.json())
     .then(showData);
 
+  /* Kalder således at hvert element i arrayet bliver kaldt og information vises for hvert element (ellers ville den kun vise for den første) */
   function showData(items) {
     items.forEach(showHerb);
   }
