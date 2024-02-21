64c0dc1b1dcaedee6552b7c9ea42fac9638ff81a
SiljeSeneberg <silj3299@tud.kea.dk>
2024-02-21 12:59:54 +0100

    link til siderne

--- js/produktliste.js
+++ js/produktliste.js
@@ -1,36 +1,72 @@
-/* const urlParms = new URLSearchParams(window.location.search);
-const season = urlParms.get("season"); */
+const urlParms = new URLSearchParams(window.location.search);
+const season = urlParms.get("season");
 
 /* `https://pliavlkcgnwhouapeprm.supabase.co/rest/v1/vildmad_data?season=eq.${season}` */
+if (season) {
+  fetch(
+    `https://pliavlkcgnwhouapeprm.supabase.co/rest/v1/vildmad_data?season=eq.${season}`,
+    {
+      method: "GET",
+      headers: {
+        apikey:
+          "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBsaWF2bGtjZ253aG91YXBlcHJtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDc0ODM2OTksImV4cCI6MjAyMzA1OTY5OX0.nkykaEzPhV7L-q--3enzm5iphogeP_xz2_IcvgkKhuM",
+      },
+    }
+  )
+    .then((res) => res.json())
+    .then(showData);
 
-fetch("https://pliavlkcgnwhouapeprm.supabase.co/rest/v1/vildmad_data", {
-  method: "GET",
-  headers: {
-    apikey:
-      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBsaWF2bGtjZ253aG91YXBlcHJtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDc0ODM2OTksImV4cCI6MjAyMzA1OTY5OX0.nkykaEzPhV7L-q--3enzm5iphogeP_xz2_IcvgkKhuM",
-  },
-})
-  .then((res) => res.json())
-  .then(showData);
+  function showData(items) {
+    items.forEach(showHerb);
+  }
 
-function showData(items) {
-  items.forEach(showHerb);
-}
+  function showHerb(item) {
+    console.log(item);
 
-function showHerb(item) {
-  console.log(item);
+    /* fanger template */
+    const template = document.querySelector("#herbs_template").content;
 
-  /* fanger template */
-  const template = document.querySelector("#herbs_template").content;
+    /* lav en kopi */
+    const copy = template.cloneNode(true);
 
-  /* lav en kopi */
-  const copy = template.cloneNode(true);
+    /* ændre indhold i template */
+    copy.querySelector("img").src = `img/herbs/${item.urtImg}`;
+    copy.querySelector("h3").textContent = item.name;
+    copy.querySelector("a").href = `produkt.html?id=${item.id}`;
 
-  /* ændre indhold i template */
-  copy.querySelector("img").src = `img/herbs/${item.urtImg}`;
-  copy.querySelector("h3").textContent = item.name;
-  copy.querySelector("a").href = `produkt.html?id=${item.id}`;
+    /* append */
+    document.querySelector(".herbs_grid").appendChild(copy);
+  }
+} else {
+  fetch("https://pliavlkcgnwhouapeprm.supabase.co/rest/v1/vildmad_data", {
+    method: "GET",
+    headers: {
+      apikey:
+        "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBsaWF2bGtjZ253aG91YXBlcHJtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDc0ODM2OTksImV4cCI6MjAyMzA1OTY5OX0.nkykaEzPhV7L-q--3enzm5iphogeP_xz2_IcvgkKhuM",
+    },
+  })
+    .then((res) => res.json())
+    .then(showData);
 
-  /* append */
-  document.querySelector(".herbs_grid").appendChild(copy);
+  function showData(items) {
+    items.forEach(showHerb);
+  }
+
+  function showHerb(item) {
+    console.log(item);
+
+    /* fanger template */
+    const template = document.querySelector("#herbs_template").content;
+
+    /* lav en kopi */
+    const copy = template.cloneNode(true);
+
+    /* ændre indhold i template */
+    copy.querySelector("img").src = `img/herbs/${item.urtImg}`;
+    copy.querySelector("h3").textContent = item.name;
+    copy.querySelector("a").href = `produkt.html?id=${item.id}`;
+
+    /* append */
+    document.querySelector(".herbs_grid").appendChild(copy);
+  }
 }
