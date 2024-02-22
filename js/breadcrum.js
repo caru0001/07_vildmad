97e046659789c978796b798483541bc31f4df87c
Malene <Maba0007@stud.kea.dk>
2024-02-22 08:45:15 +0100

    sti virker ikke optimalt

--- js/breadcrum.js
+++ js/breadcrum.js
@@ -1,30 +1,30 @@
-// JavaScript til at generere brødkrummesti
-var pathArray = window.location.pathname.split("/");
-var breadcrumbs = "";
+// // JavaScript til at generere brødkrummesti
+// var pathArray = window.location.pathname.split("/");
+// var breadcrumbs = "";
 
-for (var i = 0; i < pathArray.length; i++) {
-  if (pathArray[i] !== "") {
-    breadcrumbs += "<a href='";
+// for (var i = 0; i < pathArray.length; i++) {
+//   if (pathArray[i] !== "") {
+//     breadcrumbs += "<a href='";
 
-    for (var j = 0; j <= i; j++) {
-      breadcrumbs += "/" + pathArray[j];
-    }
+//     for (var j = 0; j <= i; j++) {
+//       breadcrumbs += "/" + pathArray[j];
+//     }
 
-    breadcrumbs += "'>" + pathArray[i] + "</a> / ";
-  }
-}
+//     breadcrumbs += "'>" + pathArray[i] + "</a> / ";
+//   }
+// }
 
-document.getElementById("breadcrumbs").innerHTML = breadcrumbs;
+// document.getElementById("breadcrumbs").innerHTML = breadcrumbs;
 
-// Tilføj en klasse til det aktuelle brødkrummelink
-var currentPath = window.location.pathname;
-var breadcrumbLinks = document.querySelectorAll("#breadcrumbs a");
+// // Tilføj en klasse til det aktuelle brødkrummelink
+// var currentPath = window.location.pathname;
+// var breadcrumbLinks = document.querySelectorAll("#breadcrumbs a");
 
-breadcrumbLinks.forEach(function (link) {
-  if (link.getAttribute("href") === currentPath) {
-    link.classList.add("breadstil");
-  }
-});
+// breadcrumbLinks.forEach(function (link) {
+//   if (link.getAttribute("href") === currentPath) {
+//     link.classList.add("breadstil");
+//   }
+// });
 
 // Definerer brødkrummestier for forskellige sider
 const breadcrumbsMap = {
@@ -48,3 +48,14 @@ function updateBreadcrumbs() {
 
 // Kald funktionen ved indlæsning af siden
 updateBreadcrumbs();
+
+function updateBreadcrumbsForProduct() {
+  const urlParams = new URLSearchParams(window.location.search);
+  const productId = urlParams.get("id");
+  const productName = getProductById(productId); // Du skal implementere getProductById-funktionen for at hente produktets navn
+
+  if (productName) {
+    const breadcrumbs = document.getElementById("breadcrumbs");
+    breadcrumbs.textContent = "Home / Herbs / " + productName;
+  }
+}
